757a27108c2836be3075288e3eb2475627a82de4
danielcastrof <daniellcastrojr@gmail.com>
2022-09-15 10:06:05 -0300

    add swagger

--- games_library/src/main.ts
+++ games_library/src/main.ts
@@ -1,9 +1,30 @@
+/* eslint-disable prettier/prettier */
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+  .setTitle('Games Library API')
+  .setDescription('API para gerenciamento de uma biblioteca de jogos')
+  .setVersion('1.0')
+  .addTag('games')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(6000);
 }
