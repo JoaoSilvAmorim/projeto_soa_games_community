3ba8b18a268b3e423eeee3d4f551cd71e1dac453
danielcastrof <daniellcastrojr@gmail.com>
2022-09-15 10:06:18 -0300

    add class-validator

--- games_library/src/game/dto/create-game.dto.ts
+++ games_library/src/game/dto/create-game.dto.ts
@@ -1,26 +1,48 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty } from "@nestjs/swagger";
+import { IsDateString, IsString } from "class-validator";
 
 export class CreateGameDto {
   id?: number;
+
   @ApiProperty({ example: 'The Last of Us', description: 'Título do jogo' })
+  @IsString()
   title: string;
-  @ApiProperty({ example: 'Ação', description: 'Gênero do jogo' })
+  
+  
+  @IsString()
+  @ApiProperty({ example: 'https://www.freetogame.com/g/517/thumbnail.jpg', description: 'Imagem do jogo' })
   thumbnail: string;
-  @ApiProperty({ example: 'Ação', description: 'Gênero do jogo' })
+  
+  @IsString()
+  @ApiProperty({ example: 'Jogo Multiplayer', description: 'Descrição do jogo' })
   short_description: string;
-  @ApiProperty({ example: 'Ação', description: 'Gênero do jogo' })
+  
+  @IsString()
+  @ApiProperty({ example: 'https://www.freetogame.com/open/lost-ark', description: 'URL do jogo' })
   game_url: string;
+  
+  @IsString()
   @ApiProperty({ example: 'Ação', description: 'Gênero do jogo' })
   genre: string;
-  @ApiProperty({ example: 'Ação', description: 'Gênero do jogo' })
+  
+  @IsString()
+  @ApiProperty({ example: 'PC', description: 'Plataforma do jogo' })
   platform: string;
-  @ApiProperty({ example: 'Ação', description: 'Gênero do jogo' })
+  
+  @IsString()
+  @ApiProperty({ example: 'Amazon Games', description: 'Fornecedora do jogo' })
   publisher: string;
-  @ApiProperty({ example: 'Ação', description: 'Gênero do jogo' })
+  
+  @IsString()
+  @ApiProperty({ example: 'Smilegate RPG', description: 'Desenvolvedora do jogo' })
   developer: string;
+  
+  @IsDateString("yyyy-MM-dd")
   @ApiProperty({ example: '2022-01-30', description: 'Data de Lançamento' })
   release_date: string;
-  @ApiProperty({ example: 'Ação', description: 'Gênero do jogo' })
+  
+  @IsString()
+  @ApiProperty({ example: 'https://www.freetogame.com/lost-ark', description: 'Link para acesso ao jogo' })
   freetogame_profile_url: string;
 }
