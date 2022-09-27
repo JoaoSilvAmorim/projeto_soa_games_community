67d5fd1f7c80b37e867de8d3dbf7ae4780a9d223
danielcastrof <daniellcastrojr@gmail.com>
2022-09-26 23:10:27 -0300

    update db

--- forum/prisma/migrations/20220916171049_update_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `answers` DROP FOREIGN KEY `Answers_topicId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `Answers` ADD CONSTRAINT `Answers_topicId_fkey` FOREIGN KEY (`topicId`) REFERENCES `Topics`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
