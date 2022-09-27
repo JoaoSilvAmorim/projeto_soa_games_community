f18a73acc3b47c2b067017abe132ab37dcc4286e
danielcastrof <daniellcastrojr@gmail.com>
2022-09-26 23:08:22 -0300

    update db

--- games_library/prisma/migrations/20220927015919_init/migration.sql
+++ games_library/prisma/migrations/20220927015919_init/migration.sql
@@ -9,8 +9,9 @@ CREATE TABLE `Game` (
     `platform` VARCHAR(800) NOT NULL,
     `publisher` VARCHAR(800) NOT NULL,
     `developer` VARCHAR(800) NOT NULL,
-    `release_date` VARCHAR(800) NOT NULL,
+    `release_date` VARCHAR(191) NOT NULL,
     `freetogame_profile_url` VARCHAR(800) NOT NULL,
 
+    UNIQUE INDEX `Game_title_key`(`title`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
